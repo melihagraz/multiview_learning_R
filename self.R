bff164d48b55a767495114253ac175604c4a0b29
Melih Agraz <melih.agraz@yahoo.com>
2022-07-25 21:11:58 +0300

    minor revision
    minor revision

--- self.R
+++ self.R
@@ -2,7 +2,6 @@
 #'
 #' @description
 #' You can apply xx learning algorithms with the \code{self} function.
-#'
 #' @param dataL tabulated labeled data
 #' @param dataU tabulated unlabeled data
 #' @param method Machine Learning methods you can select;
@@ -32,7 +31,6 @@
 #' n_fold = 5, seed = 123,...)
 #'
 #' @export
-
 #---------------------------------------------------------------------------required R libraries
 library(dplyr)
 library(caret)
@@ -44,6 +42,7 @@ library(praznik)
 library(e1071)
 library(gridExtra)
 library(PRROC)
+source("helper_functions.R")
 
 #----- Internal function ------#
 
@@ -51,8 +50,7 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
                   neg_conf_prob = 0.9, pos_conf_prob = 0.1, feature_sel = TRUE,
                   n_fold = 5, seed = 123,...) {
 
-  #--------- you can use below code to install required packages or above code
-  # 
+  #--------- you can use below code to install required packages or above library part 
   requiredLibs <- c("dplyr", "caret", "randomForest", "ggplot2", "ggpubr",
                     "ROSE", "praznik", "e1071", "gridExtra", "PRROC")
 
@@ -63,13 +61,10 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
     }
   }
 #------------------------
-  
   ###checking data
   if (missing(dataL))
     stop("labeled/unlabeled data is reaquired.\n")
   method <- match.arg(method)
-
-  source("helper_functions.R")
   #-----------------------------------------------------------------------------# FEATURE SEL
   # we have already selected the features before
   if (feature_sel) {
@@ -77,7 +72,6 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
                       "nphl_insulin_mean")
     sel_feat_out <- c("g1diabed_std", "hba1c_mean", "fpg_std",
                       "nphl_insulin_mean", "out")
-
     dataL <- dataL[sel_feat_out]
     dataU <- dataU[sel_feat]
   }
@@ -98,20 +92,17 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
   }
 
   folds <- createFolds(dataL$out, k = n_fold)
-
   datamainU<-dataU
   for (i_cv in 1:n_fold) {
     testIndex <- folds[[i_cv]]
     train <- dataL[-testIndex,]
     test  <- dataL[testIndex,]
-
     dataU<-datamainU
 
     if (imbalanced) {
       imbalanced_result<-balanced(train, seed=seed)
       train<-imbalanced_result
     }
-
     iterations = 1
     length_negpos_selection <- 1 # : is the length of selected positive and negative
     # pseudo labels.  to start to the while loop, we assign it as 1.
@@ -121,7 +112,6 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
         naiv_bay <- naiveBayes(as.factor(out) ~ ., data = train,
                                method = 'class')
         naiv_pre_unlab <- predict(naiv_bay, dataU, type = "raw")
-
       } else {
         naiv_bay <- randomForest(
           as.factor(out) ~ .,
@@ -130,7 +120,6 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
           ntree = 500
         )
         naiv_pre_unlab <- predict(naiv_bay, dataU, type = "prob")
-
       }
       confusion_result <- confusionMatrix(predict(naiv_bay, test),
                                           as.factor(test$out),
@@ -144,25 +133,20 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
       Accuracy_CV[i_cv, iterations]  <- round(confusion_result$overall[[1]], 4)
       F1_CV[i_cv, iterations] <- round(confusion_result$byClass[[7]], 4)
       BalancedAccuracy_CV[i_cv, iterations] <- round(confusion_result$byClass[[11]], 4)
-
       # CONFUSION MATRIX
       TN[i_cv, iterations] <- round(confusion_result$table[1], 4)
       TP[i_cv, iterations] <- round(confusion_result$table[4], 4)
       FN[i_cv, iterations] <- round(confusion_result$table[3], 4)
       FP[i_cv, iterations] <- round(confusion_result$table[2], 4)
-
       # Create Pseudo labels
       unlabeled_probability <- cbind(dataU, pr = naiv_pre_unlab[, 1])
       unlabeled_probability <- unlabeled_probability[order(-unlabeled_probability$pr), ]
-
       unlabeled_probability$out <- ifelse(unlabeled_probability$pr > 0.5, 0, 1)
       neg <- which(unlabeled_probability$pr > neg_conf_prob)
       length_ix_neg[i_cv, iterations] <- length(neg)
       pos <- which(unlabeled_probability$pr < pos_conf_prob)
       length_ix_pos[i_cv, iterations] <- length(pos)
-
       length_negpos_selection <- c(neg, pos)
-
       unlabeled_probability2 <- unlabeled_probability %>%
         dplyr::select(-"pr")
       unlabeled_probability3 <- unlabeled_probability2[length_negpos_selection,]
@@ -172,7 +156,6 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
 
       train <- new_labeled_data
       dataU <- unlabeled_probability4
-
       iterations = iterations + 1
     } # end of  while
   } # end of folds
@@ -186,8 +169,7 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
   # fold 3--> 12 iterations,
   # min_NA is 12
   missing_vector <- apply(TN, 1, function(x) {
-    first(which(is.na(x) == TRUE))
-  })
+    first(which(is.na(x) == TRUE))})
   min_NA <- min(missing_vector[!is.na(missing_vector)]) - 1
   # accuracy measures
   Sensitivity_CV <- Sensitivity_CV[, 1:min_NA]
@@ -203,12 +185,9 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
 
   TP <- TP[, 1:min_NA]; TN <- TN[, 1:min_NA]
   FP <- FP[, 1:min_NA]; FN <- FN[, 1:min_NA]
-
   colnames(FN) <- colnames(FP) <- colnames(TP) <- colnames(TN) <- NULL
-
   length_ix_neg <- length_ix_neg[, 1:min_NA]
   length_ix_pos <- length_ix_pos[, 1:min_NA]
-
   #-----------------------------------------------------------------------------
   TPNF <- cbind(TP, TN, FP, FN)
   colnames(TPNF) <- c(
@@ -218,10 +197,8 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
     paste0("it",  paste0(1:min_NA, c("-FN")))
   )
   rownames(TPNF) <- paste0("FOLD", 1:(n_fold))
-
   saver(TPNF,  name="ConfMat",   format =  ".csv",
-        main_method="self",
-        method = method, feature_sel = feature_sel)
+        main_method="self", method = method, feature_sel = feature_sel)
   #-----------------------------------------------------------------------------
   # function for calculating mean and sd of accuracy measures
   #-----------------------------------------------------------------------------# GRAPH:ACCURACY MEASURES
@@ -233,45 +210,38 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   # Specifity graph
   SpecCV_fin <- data_summary(Specificity_CV)
   plot_self( SpecCV_fin, name="Specificity",
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   # PPV graph
   PPVCV_fin <- data_summary(PPV_CV)
   plot_self( PPVCV_fin, name="PPV",
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   # NPV graph
   NPVCV_fin <- data_summary(NPV_CV)
   plot_self( NPVCV_fin, name="NPV",
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   #  ACC graph
   AccCV_fin <- data_summary(Accuracy_CV)
   plot_self( AccCV_fin, name="Accuracy",
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   # balanced accuracy graph
   BalAccCV_fin <- data_summary(BalancedAccuracy_CV)
   plot_self( BalAccCV_fin, name="BalancedAccuracy",
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   # F1 graph
   F1CV_fin <- data_summary(F1_CV)
-
   plot_self(F1CV_fin, name="F1",
              number_iterations=Nit,
              method = method,
@@ -283,7 +253,6 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   # TN
   TN_fin <- data_summary(TN)
   plot_self( TN_fin, name="TN",
@@ -296,14 +265,12 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   #FN
   FN_fin <- data_summary(FN)
   plot_self( FN_fin, name="FN",
              number_iterations=Nit,
              method = method,
              feature_sel = feature_sel, main_method="Self", format=".pdf")
-
   #-----------------------------------------------------------------------------# GRAPH: Pos_Neg Sel
   # how many positive and negative output was selected in each iteration and
   # graph for positive and negative selection in each iteration
@@ -314,8 +281,8 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
     View = c(rep("Positive", (Nit)), rep("Negative", (Nit))),
     Iterations = c(1:(Nit), 1:(Nit)),
     Mean = c(length_ix_pos2[, 2], length_ix_neg2[, 2]),
-    Sd = c(length_ix_pos2[, 3], length_ix_neg2[, 3])
-  )
+    Sd = c(length_ix_pos2[, 3], length_ix_neg2[, 3]) )
+  
   pos_neg_pl <- ggplot(pos_neg_fin, aes(
     x = Iterations,
     y = Mean,
@@ -328,11 +295,8 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
     theme_minimal() +
     ylab("Selected Positivie Negative Values") +
     theme_bw()
-
   saver(pos_neg_pl,  name="PosNeg",   format =  ".pdf",
-        main_method="self",
-        method = method, feature_sel = feature_sel)
-
+        main_method="self",  method = method, feature_sel = feature_sel)
   # the final output we want to return
   first_last <- data.frame(
     Sensitivity = c(SensCV_fin[1, 2],
@@ -362,14 +326,11 @@ self  <- function(dataL, dataU, method = c("nb", "rf"), imbalanced = TRUE,
     BalAcc = c(BalAccCV_fin[1, 2],
                BalAccCV_fin[dim(BalAccCV_fin)[1], 2],
                100 * ((BalAccCV_fin[dim(BalAccCV_fin)[1], 2] -
-                         BalAccCV_fin[1, 2]) / (BalAccCV_fin[1, 2])))
-  )
-
+                         BalAccCV_fin[1, 2]) / (BalAccCV_fin[1, 2]))))
   rownames(first_last) <- c("First iteration", "Last iteration", "Percentage")
-
   saver( first_last,  name="FirstLast",   format =  ".csv",
          main_method="self",
          method = method, feature_sel = feature_sel)
-
+  
   return(first_last = first_last)
 }
