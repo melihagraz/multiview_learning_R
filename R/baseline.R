bacdbe4e39b25be7b05af7053d6d9962becceab2
Melih Agraz <melih.agraz@yahoo.com>
2022-11-10 21:13:42 +0300

    ROC
    baseline ROC AN updated

--- R/baseline.R
+++ R/baseline.R
@@ -82,13 +82,20 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
   CV.Sensitivity <- CV.Specifity <- numeric()
   CV.PPV <- CV.NPV <- numeric()
   CV.AUC <- CV.Accuracy <- CV.F.score <- numeric()
-
+  
+  roc_pool_sens <-c()
+  roc_pool_spec <-numeric()
+  roc_catg<-c()
+  test_dim<-c()
+  len_sens<-c()
+ 
   if (!is.null(seed)){
     set.seed(seed)
   }
   folds <- createFolds(dataL$out, k = n_fold)
   method <- match.arg(method)
-
+  
+i_cv<-5
   for (i_cv in 1:n_fold) {
 
     testIndex <- folds[[i_cv]]
@@ -99,6 +106,8 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
     }
     trainset <- dataL[-testIndex, ]
     testset  <- dataL[ testIndex, ]
+    test_dim[i_cv]<-dim(testset)[1]
+
 
     if (!is.null(seed)){
       set.seed(seed)
@@ -112,11 +121,25 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
       modelFit <- glm(out ~ ., data = trainset, family = "binomial")
       predicted <- modelFit %>%
         predict(testset, type = "response")
+
       par(pty = "m")
-      auc_res<-roc(testset$out, predicted, plot=TRUE,print.auc=TRUE)
+      auc_res<-roc(testset$out, predicted, plot=FALSE,
+                   print.auc=TRUE)
+      auc_res$percent
+      auc_res$sensitivities
+      auc_res$specificities
+      roc_pool_sens<-append(roc_pool_sens, auc_res$sensitivities)
+      roc_pool_spec<-append(roc_pool_spec, auc_res$specificities)
+      roc_catg     <-append(roc_catg, rep(paste0("ROC", i_cv), 
+                                          length(auc_res$sensitivities)))
+      
+      len_sens[i_cv]<-length(auc_res$sensitivities)
       CV.AUC[i_cv]<-auc_res$auc[1]
       predicted <- ifelse(predicted > 0.5, 1, 0)
+    
     }
+
+    
 
     if (method == "nb") {
       #------------------------------------------------------------------------# Naive Bayes model traing
@@ -125,8 +148,17 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
                              usekernel = T)
       par(pty = "m")
       predicted <- predict(modelFit, testset)
-      predict_num<-as.numeric(as.character(predicted))
-      auc_res<-roc(testset$out, predict_num, plot=TRUE,print.auc=TRUE)
+      predicted_pr <- predict(modelFit, testset, type="raw")
+ 
+      auc_res<-roc(testset$out,     predicted_pr[,1], plot=FALSE, 
+                   print.auc=TRUE)
+      auc_res$specificities
+      
+      roc_pool_sens<-append(roc_pool_sens, auc_res$sensitivities)
+      roc_pool_spec<-append(roc_pool_spec, auc_res$specificities)
+      roc_catg     <-append(roc_catg, rep(paste0("ROC", i_cv), 
+                                          length(auc_res$sensitivities)))
+      len_sens[i_cv]<-length(auc_res$sensitivities)
       CV.AUC[i_cv]<-auc_res$auc[1]
     }
     if (method == "xgb") {
@@ -140,9 +172,21 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
       predicted <- predict(modelFit,
                            newdata =  as.matrix(testset[-x6]),
                            type = "response")
+      
+      predicted_pr <- predict(modelFit,
+                           newdata =  as.matrix(testset[-x6]),
+                           type = "raw")
+      
       par(pty = "m")
       auc_res<-roc(testset$out, 
-                   predicted, plot=TRUE,print.auc=TRUE)
+                   predicted_pr, plot=FALSE,print.auc=TRUE)
+      auc_res
+      roc_pool_sens<-append(roc_pool_sens, auc_res$sensitivities)
+      roc_pool_spec<-append(roc_pool_spec, auc_res$specificities)
+      roc_catg     <-append(roc_catg, rep(paste0("ROC", i_cv), 
+                                          length(auc_res$sensitivities)))
+      len_sens[i_cv]<-length(auc_res$sensitivities)
+      
       CV.AUC[i_cv]<-auc_res$auc[1]
       predicted <- ifelse(predicted > 0.5, 1, 0)
     }
@@ -157,7 +201,14 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
 
       predicted <- predict(modelFit, testset)
       predict_num<-as.numeric(as.character(predicted))
-      auc_res<-roc(testset$out, predict_num, plot=TRUE,print.auc=TRUE)
+      auc_res<-roc(testset$out, predict_num, plot=FALSE,print.auc=TRUE)
+      
+      roc_pool_sens<-append(roc_pool_sens, auc_res$sensitivities)
+      roc_pool_spec<-append(roc_pool_spec, auc_res$specificities)
+      roc_catg     <-append(roc_catg, rep(paste0("ROC", i_cv), 
+                                          length(auc_res$sensitivities)))
+      len_sens[i_cv]<-length(auc_res$sensitivities)
+      
       CV.AUC[i_cv]<-auc_res$auc[1]
     }
     if (method == "rf") {
@@ -171,13 +222,18 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
       par(pty = "m")
       predicted <- predict(modelFit, testset, type = "class")
       predict_num<-as.numeric(as.character(predicted))
-      auc_res<-roc(testset$out, predict_num, plot=TRUE,
+      auc_res<-roc(testset$out, predict_num, plot=FALSE,
                    print.auc=TRUE)
+      roc_pool_sens<-append(roc_pool_sens, auc_res$sensitivities)
+      roc_pool_spec<-append(roc_pool_spec, auc_res$specificities)
+      roc_catg     <-append(roc_catg, rep(paste0("ROC", i_cv), 
+                                          length(auc_res$sensitivities)))
+      len_sens[i_cv]<-length(auc_res$sensitivities)
       CV.AUC[i_cv]<-auc_res$auc[1]
     }
 
-    actual   <- as.factor(testset$out)
-    predicted<- as.factor(predicted)
+    actual    <- as.factor(testset$out)
+    predicted <- as.factor(predicted)
   
     perfMeas <- confMat(actual, predicted, positive = "1")
 
@@ -189,6 +245,44 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
     CV.F.score[i_cv] = perfMeas[["f1"]]
     
   }# end fold
+
+dat<-data.frame(spec=roc_pool_spec,
+                sens=roc_pool_sens,
+                roc=roc_catg)
+
+
+f1<-len_sens[1]
+f2<-len_sens[2]
+f3<-len_sens[3]
+f4<-len_sens[4]
+f5<-len_sens[5]
+
+dat1<-  dat[1:(f1+1),]
+dat2<-  dat[((f1+1+1):(f1+1+f2+1)),]
+dat3<-  dat[(f1+1+f2+1+1):(f1+1+f2+1+f3+1),]
+dat4<-  dat[(f1+1+f2+1+f3+1+1):(f1+1+f2+1+f3+1+f4+1),]
+dat5<-  dat[(f1+1+f2+1+f3+1+f4+1+1):(f1+f2+f3+f4+f5+1),]
+
+
+
+gg_ROC<-ggplot()+
+  geom_line(data=dat1, aes(1-spec, sens, color="ROC1"),size = 0.5, alpha = 0.5)+
+  geom_line(data=dat2, aes(1-spec, sens,color="ROC2"),size = 0.5, alpha = 0.7)+
+  geom_line(data=dat3, aes(1-spec, sens,color="ROC3"),size = 0.8, alpha = 0.7)+
+  geom_line(data=dat4, aes(1-spec, sens,color="ROC4"),size = 0.8, alpha = 0.7)+
+  geom_line(data=dat5, aes(1-spec, sens,color="ROC5"),size = 0.8, alpha = 0.7)+
+  scale_color_manual(values = c(
+    'ROC1' = "#C4961A",
+    'ROC2' = "#D16103",
+    'ROC3' = "#52854C",
+    'ROC4' = "#4E84C4",
+    'ROC5' = "#293352")) +
+  labs(color = paste( "mean AUC", round(mean( CV.AUC),4)),
+       x = "False Positive Rate (1-Specificity)", 
+       y = "True Positive Rate (Sensitivity)"  )+
+  theme(legend.position = c(.8, .2))
+
+
   res <- matrix(c(
     mean(CV.NPV, na.rm=TRUE ),
     mean(CV.PPV),
@@ -200,5 +294,5 @@ ConventionalMacLearn <- function(dataL, method = c("logreg", "nb", "rf", "svm", 
   ), nrow = 1)
   colnames(res) <- c("NPV", "PPV", "Spec", "Sens", "Acc", "F1", "AUC")
 
-  return(result = res)
+  return(result = list(res=res, plot=gg_ROC))
 }
